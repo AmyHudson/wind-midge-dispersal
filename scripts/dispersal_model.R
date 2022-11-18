d23614dd38ba84996a7b2fcd279426e4fbc115e3
Amy Hudson <amyhud7@gmail.com>
2022-11-18 10:53:47 -0500

    Update dispersal_model.R

--- scripts/dispersal_model.R
+++ scripts/dispersal_model.R
@@ -12,21 +12,40 @@ library(splitr)
 library(lubridate)
 library(here)
 library(magrittr)
+library(mapview)
+
+
 #################
+# rate of 5 mass units per hour
+
+# density (in grams per cubic centimeter) 
+
+# shape factor (value from 0 to 1)
+# Female sonorensis are ~3mm long x 0.5 mm wide x 0.5 mm tall.
+# A shape factor is designed to prevent the creation of irregularly shaped lots by providing a measurement to evaluate the compactness and degree of regularity of the shape of a lot. 
+# Shape factor (SF) is the non-dimensional ratio of the lot perimeter (P) squared, divided by the lot area (A), where P and A are derived from the same unit of measurement. 
+# SF = (P^2/A)
+# 3mm x 0.5mm x 0.5mm 
+
+
+# ddep_mw molecular weight in units of g/mol
+# unfed females: 0.00025 g 
+# blood fed: 0.00057 g
+
 dispersion_model <-
   create_dispersion_model() %>%
   add_source(
     name = "particle",
-    lat = 33.99, lon = -117.60, height = 50,
-    rate = 5, pdiam = 15, density = 1.5, shape_factor = 0.8,
-    release_start = lubridate::ymd_hm("2018-09-15 05:00"),
-    release_end = lubridate::ymd_hm("2018-09-15 05:00") + lubridate::hours(4)
+    lat = 34.0, lon = -117.0, height = 10,
+    #rate = 5, pdiam = 15, density = 1.5, shape_factor = 0.8,
+    release_start = lubridate::ymd_hm("2018-09-10 17:00"),
+    release_end = lubridate::ymd_hm("2018-09-10 17:00") + lubridate::hours(4)
   ) %>%
   add_dispersion_params(
-    start_time = lubridate::ymd_hm("2018-09-15 05:00"),
-    end_time = lubridate::ymd_hm("2018-09-15 05:00") + lubridate::hours(24),
+    start_time = lubridate::ymd_hm("2018-09-10 17:00"),
+    end_time = lubridate::ymd_hm("2018-09-10 17:00") + lubridate::hours(12),
     direction = "forward", 
-    met_type = "reanalysis"
+    met_type = "nam12",
   ) %>%
   run_model()
 
@@ -34,32 +53,14 @@ dispersion_model <-
 dispersion_tbl <- dispersion_model %>% get_output_tbl()
 
 # Plot particle data onto a map
-dispersion_model %>% dispersion_plot()
+p <- dispersion_model %>% dispersion_plot()
 
-
-#################
-dispersion_model <-
-  create_dispersion_model() %>%
-  add_source(
-    name = "particle",
-    lat = 49.0, lon = -123.0, height = 50,
-    rate = 5, pdiam = 15, density = 1.5, shape_factor = 0.8,
-    release_start = lubridate::ymd_hm("2015-07-01 00:00"),
-    release_end = lubridate::ymd_hm("2015-07-01 00:00") + lubridate::hours(2)
-  ) %>%
-  add_dispersion_params(
-    start_time = lubridate::ymd_hm("2015-07-01 00:00"),
-    end_time = lubridate::ymd_hm("2015-07-01 00:00") + lubridate::hours(6),
-    direction = "forward", 
-    met_type = "reanalysis",
-  ) %>%
-  run_model()
-
-# Get a tibble containing the model results
-dispersion_tbl <- dispersion_model %>% get_output_tbl()
-
-# Plot particle data onto a map
-dispersion_model %>% dispersion_plot()
+#remotes::install_github("r-spatial/mapview")
+mapviewOptions(fgb = FALSE)
+remotes::install_github('rstudio/rmarkdown')
+library(mapview)
+mapshot(p, 
+        file = "figures/dispersion.png")
 
 #################
 # doesn't work yet
