2e27cc04a781b2981fcf9deb799af690f43b0551
Amy Hudson <amyhud7@gmail.com>
2023-04-04 11:39:38 -0500

    working on dispersal model

--- scripts/dispersal_model.R
+++ scripts/dispersal_model.R
@@ -1,12 +1,14 @@
-# dispersal modeling at California site based on derived parameter distributions
+#NOT RUNNING DEFAULT SCRIPT 3/23- try another default script??
 
-This run has been set to be modeled for 6 h. The starting location of 49.0ºN and 123.0ºW is set using lat = 49.0 and lon = -123.0; the starting height of 50 m above ground level is set by height = 50. The meteorological options include the type of met data to use (global NCEP Reanalysis data is used here with met_type = "reanalysis).
+# Dispersal modeling at California site based on derived parameter distributions
 
-A single emissions species is set to be emitted (using add_source()) for 2 hours at an emission rate of 5 mass units per hour (rate = 5). 
-Emissions begin at the same time as the start of the model (release_start = lubridate::ymd_hm("2015-07-01 00:00")). 
-particle diameter (in micrometers), 
-density (in grams per cubic centimeter) 
-shape factor (value from 0 to 1)
+# Default model parameters and explanations: This run has been set to be modeled for 6 h. The starting location of 49.0ºN and 123.0ºW is set using lat = 49.0 and lon = -123.0; the starting height of 50 m above ground level is set by height = 50. The meteorological options include the type of met data to use (global NCEP Reanalysis data is used here with met_type = "reanalysis).
+
+# A single emissions species is set to be emitted (using add_source()) for 2 hours at an emission rate of 5 mass units per hour (rate = 5). 
+# Emissions begin at the same time as the start of the model (release_start = lubridate::ymd_hm("2015-07-01 00:00")). 
+# particle diameter (in micrometers), 
+# density (in grams per cubic centimeter) 
+# shape factor (value from 0 to 1)
 
 #From Phillip
 #Attached is the collection data and GPS coordinates for the 3-year population study in California.
@@ -21,7 +23,25 @@ library(lubridate)
 library(here)
 library(magrittr)
 library(mapview)
-
+#################
+dispersion_model <-
+  create_dispersion_model() %>%
+  add_source(
+    name = "particle",
+    lat = 49.0, lon = -123.0, height = 50,
+    rate = 5, pdiam = 15, density = 1.5, shape_factor = 0.8,
+    release_start = lubridate::ymd_hm("2015-07-01 00:00"),
+    release_end = lubridate::ymd_hm("2015-07-01 00:00") + lubridate::hours(2)
+  ) %>%
+  add_dispersion_params(
+    start_time = lubridate::ymd_hm("2015-07-01 00:00"),
+    end_time = lubridate::ymd_hm("2015-07-01 00:00") + lubridate::hours(6),
+    direction = "forward", 
+    met_type = "reanalysis",
+    #met_dir = here::here("met"),
+    #exec_dir = here::here("out")
+  ) %>%
+  run_model()
 
 #################
 # rate of XX mass units per hour
@@ -35,23 +55,27 @@ library(mapview)
 # SF = (P^2/A)
 # 3mm x 0.5mm x 0.5mm 
 
-
 # ddep_mw molecular weight in units of g/mol
 # unfed females: 0.00025 g 
 # blood fed: 0.00057 g
+# 50% water (18.01528 g/mol) + 50%(15% chitin (203.1925 g/mol)+ protein + fat)
+
+
+
+
 
 dispersion_model <-
   create_dispersion_model() %>%
   add_source(
     name = "particle",
-    lat = 34.0, lon = -117.0, height = 10,
+    lat = 34.0, lon = -117.5, height = 10,
     #rate = 5, pdiam = 15, density = 1.5, shape_factor = 0.8,
     release_start = lubridate::ymd_hm("2018-09-10 17:00"),
-    release_end = lubridate::ymd_hm("2018-09-10 17:00") + lubridate::hours(4)
+    release_end = lubridate::ymd_hm("2018-09-10 17:00") + lubridate::hours(1)
   ) %>%
   add_dispersion_params(
     start_time = lubridate::ymd_hm("2018-09-10 17:00"),
-    end_time = lubridate::ymd_hm("2018-09-10 17:00") + lubridate::hours(12),
+    end_time = lubridate::ymd_hm("2018-09-10 17:00") + lubridate::hours(2),
     direction = "forward", 
     met_type = "nam12",
   ) %>%
