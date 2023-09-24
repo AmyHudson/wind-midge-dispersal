9f5b7c85a62d79dc0ba5efed6ac2a06326210678
Amy Hudson <amyhud7@gmail.com>
2023-09-24 12:33:34 -0500

    added wind file

--- scripts/era5_uwndspeed_height_longitude.R
+++ scripts/era5_uwndspeed_height_longitude.R
@@ -107,7 +107,7 @@ colnames(d5) <- lon
 d5$lev <- lev1
 pd <- pivot_longer(d5,cols = 1:128,values_to = "uwndSpeed", names_to = "lon")
 pd$lon <- as.numeric(pd$lon)
-pd$facet <- rep(month.name[k],dim(pd)[1]) #rep(paste(month.name[k],utod[j]),dim(pd)[1])
+pd$facet <- rep(month.abb[k],dim(pd)[1]) #rep(paste(month.name[k],utod[j]),dim(pd)[1])
 
 pd_total <- rbind(pd_total,pd)
 
@@ -129,9 +129,9 @@ pd_total <- pd_total %>%
          lev = as.factor(lev)) #, levels = as.character(rev(lev1))
 
 
-png("figures/era5uwndclimatology.png",
-    width = 8, height = 11, units = "in", 
-    pointsize = 10, res = 600)
+#png("figures/era5uwndclimatology.png",
+#    width = 8, height = 11, units = "in", 
+#    pointsize = 10, res = 600)
 
 ggplot(pd_total, aes(lon, lev, fill= uwndSpeed)) + 
   geom_tile() +
@@ -139,13 +139,76 @@ ggplot(pd_total, aes(lon, lev, fill= uwndSpeed)) +
   scale_y_discrete(limits=rev, name = "hPa")+
   #scale_y_reverse(expand = c(0, 0), name = "hPa"#,sec.axis = sec_axis(name="m")
   #)+
-  scale_fill_distiller(palette = "RdBu", limit = c(-4,4)) +
-  scale_x_continuous(guide = guide_axis(angle = 90),expand = c(0, 0))+
+  scale_fill_distiller(palette = "RdBu", limit = c(-4,4), oob = scales::oob_squish,
+                       guide = my_triangle_colourbar()) +
+  #scale_x_continuous(guide = guide_axis(angle = 90),expand = c(0, 0))+
   #ggtitle(paste(month.name[k],utod[j]))+
   theme_classic()+
-  facet_wrap(~ factor(facet, levels=month.name))
+  labs(fill='Average\nU-wind\nSpeed\n(m/s)') +
+  facet_wrap(~ factor(facet, levels=month.abb))
 
-dev.off()
+ggsave('figures/era5uwndclimatology7am.pdf',
+       width = 6,
+       height = 6,
+       units = "in")
+
+#dev.off()
+###############
+## add triangle legend?
+library(ggplot2)
+library(gtable)
+library(grid)
+
+my_triangle_colourbar <- function(...) {
+  guide <- guide_colourbar(...)
+  class(guide) <- c("my_triangle_colourbar", class(guide))
+  guide
+}
+
+guide_gengrob.my_triangle_colourbar <- function(...) {
+  # First draw normal colourbar
+  guide <- NextMethod()
+  # Extract bar / colours
+  is_bar <- grep("^bar$", guide$layout$name)
+  bar <- guide$grobs[[is_bar]]
+  extremes <- c(bar$raster[1], bar$raster[length(bar$raster)])
+  # Extract size
+  width  <- guide$widths[guide$layout$l[is_bar]]
+  height <- guide$heights[guide$layout$t[is_bar]]
+  short  <- min(convertUnit(width, "cm",  valueOnly = TRUE),
+                convertUnit(height, "cm", valueOnly = TRUE))
+  # Make space for triangles
+  guide <- gtable_add_rows(guide, unit(short, "cm"),
+                           guide$layout$t[is_bar] - 1)
+  guide <- gtable_add_rows(guide, unit(short, "cm"),
+                           guide$layout$t[is_bar])
+  
+  # Draw triangles
+  top <- polygonGrob(
+    x = unit(c(0, 0.5, 1), "npc"),
+    y = unit(c(0, 1, 0), "npc"),
+    gp = gpar(fill = extremes[1], col = NA)
+  )
+  bottom <- polygonGrob(
+    x = unit(c(0, 0.5, 1), "npc"),
+    y = unit(c(1, 0, 1), "npc"),
+    gp = gpar(fill = extremes[2], col = NA)
+  )
+  # Add triangles to guide
+  guide <- gtable_add_grob(
+    guide, top, 
+    t = guide$layout$t[is_bar] - 1,
+    l = guide$layout$l[is_bar]
+  )
+  guide <- gtable_add_grob(
+    guide, bottom,
+    t = guide$layout$t[is_bar] + 1,
+    l = guide$layout$l[is_bar]
+  )
+  
+  return(guide)
+}
+
 
 ###############
 # API script I should try sometime...
